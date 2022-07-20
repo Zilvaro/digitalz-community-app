368985a4d19283865c12cdbfd7ac6a437de94dc4
Zilvaro <zilvinas@digitalcase.ch>
2022-07-20 12:38:43 +0000

    Secret_key added to git & Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY', 'django-insecure-v!36u8i_dco(*hnpu*60afb09^3cbwc^$-)v@sticc=&3xcso%')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
