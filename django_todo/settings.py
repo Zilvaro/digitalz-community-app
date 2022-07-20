5ab049a79f7f81805450c9c4bb9d5598e24308fc
Zilvaro <zilvinas@digitalcase.ch>
2022-07-20 04:07:23 +0000

    Allowed host added

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v!36u8i_dco(*hnpu*60afb09^3cbwc^$-)v@sticc=&3xcso%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['digitalz-community-app.herokuapp.com']
 
 
 # Application definition
